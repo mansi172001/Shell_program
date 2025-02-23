4009d4cc7dce59933f876cf0fd8bd704d5233544
Mansi's projects <101920126+mansi172001@users.noreply.github.com>
2025-02-23 21:24:03 +0530

    Create Even or odd.sh

---
+++ Even or odd.sh
@@ -0,0 +1,9 @@
+# Write a shell program to check whether a number is even or odd
+echo "Enter a number:"
+read num
+
+if [ $((num % 2)) -eq 0 ]; then
+    echo "$num is Even"
+else
+    echo "$num is Odd"
+fi

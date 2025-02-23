46d5ff4ccff8e8cf61c295e327f6662a0cca1ea1
Mansi's projects <101920126+mansi172001@users.noreply.github.com>
2025-02-23 21:25:09 +0530

    Create Gross_salary.sh

---
+++ Gross_salary.sh
@@ -0,0 +1,9 @@
+# shell program to find gross salary of an employee. 
+echo -n "Enter Basic Salary: "
+read basic
+
+da=$((basic * 40 / 100)) 
+hra=$((basic * 20 / 100)) 
+gross_salary=$((basic + da + hra))
+
+echo "Gross Salary: $gross_salary"

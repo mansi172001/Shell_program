b4cdb6d3f484bbfecb1528b1cad55fc10c41a3df
Mansi's projects <101920126+mansi172001@users.noreply.github.com>
2025-02-23 21:27:45 +0530

    Create factorial.sh

---
+++ factorial.sh
@@ -0,0 +1,11 @@
+# shell program to find factorial of a number. 
+echo -n "Enter a number: "
+read num
+fact=1
+
+for (( i=1; i<=num; i++ ))
+do
+  fact=$((fact * i))
+done
+
+echo "Factorial of $num is $fact"

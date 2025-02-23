2b101b50fdd6cf8c3bd9669f1bed33c754b1ab14
Mansi's projects <101920126+mansi172001@users.noreply.github.com>
2025-02-23 21:27:21 +0530

    Create Week.sh

---
+++ Week.sh
@@ -0,0 +1,14 @@
+# Write a shell program to type number 1 to 7 and then print its corresponding day of week
+echo "Enter a number (1-7):"
+read num
+
+case $num in
+    1) echo "Monday" ;;
+    2) echo "Tuesday" ;;
+    3) echo "Wednesday" ;;
+    4) echo "Thursday" ;;
+    5) echo "Friday" ;;
+    6) echo "Saturday" ;;
+    7) echo "Sunday" ;;
+    *) echo "Invalid Input" ;;
+esac
